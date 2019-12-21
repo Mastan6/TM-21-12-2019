99f1e724ed0a900e31b32632aacb28fb547bfa29
Hidayat <Hidayat@Hidayat-PC>
2019-12-21 09:47:59 +0530

    First Commit- 21/12/2019

---
+++ src/test/java/TM_Selenium_HRM/TM_Selenium_HRM/HRM_Login.java
@@ -0,0 +1,20 @@
+package TM_Selenium_HRM.TM_Selenium_HRM;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HRM_Login {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","F:\\Softwares\\SeleniumProject\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://testingmasters.com/hrm");
+	}
+
+}
